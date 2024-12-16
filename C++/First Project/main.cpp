668706fde392423e98c8882c0dd9722caa193a4d
scalmueti <scalmueti@gmail.com>
2024-12-16 13:44:54 -0500

    Fixed g++ compiler PATH issues on Windows device. Will attempt DLL Injector soon.

--- C++/First Project/main.cpp
+++ C++/First Project/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 
 int main() {
+    using namespace std;
 
-    std::cout << "Hello world!" << std::endl;
-    std::cout << "What is your name?" << std::endl;
-    std::string userName;
-    std::cin >> userName;
-    std::cout << "Hello " << userName;
+    cout << "Hello world!" << endl;
+    cout << "What is your name?" << endl;
+    string userName;
+    cin >> userName;
+    cout << "Hello " << userName;
+    for (int a=2;a<=10;a+=3) {
+        cout << a << endl;
+    }
+    cout << "Break" << endl;
+    for(int i=2;i<=10;i+=3) {
+        cout << i << endl;
+    }
     return 0;
     
 }
